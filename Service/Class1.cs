de7fec235e6bc666b659bcda57f6b1aea21be342
ifSaZy <65533483+ifSaZy@users.noreply.github.com>
2022-04-04 09:29:45 +0300

    Test
    test

---
+++ Service/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Storage;
+
+namespace Service
+{
+    public class Class1
+    {
+        public static bool login(string login, string passsword)
+        {
+            ModelDBContainer DataBase = new ModelDBContainer();
+            var user = DataBase.Users.FirstOrDefault(x => x.Login == login);
+
+            if (user == null)
+            {
+                return false;
+            }
+            else
+            {
+                if (user.Password == passsword)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }    
+            }
+        }
+    }
+}
